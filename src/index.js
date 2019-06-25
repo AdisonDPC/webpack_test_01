ed5b028d9ad1a299946204f73a675efd0b4412f1
Adison Daniel Pérez Caraballo <adisondpc@gmail.com>
2019-06-25 03:13:39 +0100

    Update Commit - 25/06/2019 - First Commit 01.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import style from "./css/style.css"
+
+import script from "./js/script.js"
+
+console.log('Hello World from Webpack.');
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'), 
+      MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: { minimize: false }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader' ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chuckFilename: '[id].css'
+        })
+    ]
+};
